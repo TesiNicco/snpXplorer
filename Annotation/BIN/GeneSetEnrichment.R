fdf5fc6e1b94f4ddd31ec3c868bbd99f9b5a2380
Niccolo Tesi <tesinicco@gmail.com>
2024-12-17 15:37:16 +0100

    Update GeneSetEnrichment.R
    evidence codes=TRUE --> will report genes in each pathway that overlap

--- Annotation/BIN/GeneSetEnrichment.R
+++ Annotation/BIN/GeneSetEnrichment.R
@@ -20,7 +20,7 @@ overlapAnalysis <- function(i, gene_list, source_gset){
   g <- gene_list[[i]]
   g = unique(g)
   res <- suppressMessages(gprofiler2::gost(g, organism = "hsapiens", ordered_query = FALSE, multi_query = FALSE, significant = FALSE, exclude_iea = TRUE,
-                               measure_underrepresentation = FALSE, evcodes = FALSE, user_threshold = 1, correction_method = "fdr",
+                               measure_underrepresentation = FALSE, evcodes = TRUE, user_threshold = 1, correction_method = "fdr",
                                domain_scope = "annotated", custom_bg = NULL, numeric_ns = "", sources = source_gset))
   return(res$result)
 }
